9ab51ad1efb0521b5b94161c30dc520872bf0702
Claudio Bianucci <resistencia@live.de>
2014-01-16 10:14:22 +0100

    added bcm library and makefile to compile it. notice that current makefile status expects bcm library to contain the notmain()

---
+++ bcm2835/blink.c
@@ -0,0 +1,19 @@
+#include "bcm2835.c"
+
+int notmain(void){
+	uint8_t pin;
+
+	pin = RPI_V2_GPIO_P1_07;
+
+    bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
+    
+    
+	while(1){
+        bcm2835_gpio_set(pin);
+        bcm2835_delayMicroseconds(1000000);
+        bcm2835_gpio_clr(pin);
+        bcm2835_delayMicroseconds(1000000);
+	}
+	
+	return 0;
+}
